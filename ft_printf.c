c0fa8c2f590b9dee980eee6a75c64200fa6f0bae
Tom BIHOUES <Tom.bihouess@gmail.com>
2023-11-06 18:28:41 +0100

    debut

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,12 @@
 /*   By: tom <tom@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 17:09:00 by tom               #+#    #+#             */
-/*   Updated: 2023/11/06 17:12:31 by tom              ###   ########.fr       */
+/*   Updated: 2023/11/06 18:26:10 by tom              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
+#include "libft.h"
 
 int	ft_strlen(char *s)
 {
@@ -19,5 +20,20 @@ int	ft_strlen(char *s)
 	 i = 0;
 	 while (s[i])
 		i++;
+	return (i);
+}
+
+int	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		write (1, &str[i], 1);
+			i++;
+	}
 	return (i);
 }
