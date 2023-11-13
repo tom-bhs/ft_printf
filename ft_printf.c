de5b4a7c61f5e9968ac53c769b90bb829a5bb0d4
Tom BIHOUES <Tom.bihouess@gmail.com>
2023-11-13 19:30:45 +0100

    add

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tom <tom@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 17:09:00 by tom               #+#    #+#             */
-/*   Updated: 2023/11/08 16:45:34 by tom              ###   ########.fr       */
+/*   Updated: 2023/11/13 19:30:26 by tom              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,31 @@ int	ft_putstr(char *str)
 		i++;
 	}
 	return (i);
+}
+
+int	ft_format(va_list args, const char format)
+{
+	int	len;
+
+	len = 0;
+	if (format == 'c')
+		len += ft_putchar(va_arg(args, int));
+	else if (format == 's')
+		len += ft_putstr(va_arg(args, char *));
+	else if (format == 'p')
+		len += ft_nbrbase(va_arg(args, unsigned long), "0123456789abcdef",
+				true);
+	else if (format == 'd' || format == 'i')
+		len += ft_putnbr(va_arg(args, int));
+	else if (format == 'u')
+		len += ft_putnbr_un(va_arg(args, unsigned int));
+	else if (format == 'x')
+		len += ft_nbrbase(va_arg(args, unsigned int), "0123456789abcdef",
+				false);
+	else if (format == 'X')
+		len += ft_nbrbase(va_arg(args, unsigned int), "0123456789ABCDEF",
+				false);
+	else if (format == '%')
+		len += ft_putchar('%');
+	return (len);
 }
